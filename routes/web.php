854302bef6e4ad7bd37f2da2a2f45bf1633011a5
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-08 17:57:14 -0600

    feat(Rutas-Recurso): ✨route::recourse was used and we can see the crud

--- routes/web.php
+++ routes/web.php
@@ -7,3 +7,6 @@ Route::get('/', function () {
 });
 
 Route::get('/test', [\App\Http\Controllers\PrimerControlador::class, 'index']);
+
+Route::resource('post',\App\Http\Controllers\PrimerControlador::class);
+//Route::resource('category',\App\Http\Controllers\PrimerControlador::class);
