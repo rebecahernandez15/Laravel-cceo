7855dc8caa0e936c3acc8c7922bbcbb004098342
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-08 17:46:34 -0600

    feat(Controlador): ✨We did the first steps of the controller

--- routes/web.php
+++ routes/web.php
@@ -6,16 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route:: get('/contact',function(){
-
-    //return redirect('/contact2',303);
-    //return redirect()->route('contact2');
-    //return to_route('contact2');
-
-    return view('contact', ['name' => 'Rebeca']);
-})->name('contact');
-
-Route:: get('/contact2',function(){
-    return view('contact2', ['name' => 'Ximena']);
-})->name('contact2');
-
+Route::get('/test', [\App\Http\Controllers\PrimerControlador::class, 'index']);
