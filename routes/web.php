a3e793890db26f8a0c92ead965e3032cbc0652b3
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-09 10:45:36 -0600

    feat(Cambios): ✨The program was cleared to create new exercises.

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,3 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/test', [\App\Http\Controllers\PrimerControlador::class, 'index']);
-Route::get('otro/{post}/{otro?}', [\App\Http\Controllers\PrimerControlador::class, 'otro']);
-//http://testlara.test/otro/5
-
-//Route::resource('post',\App\Http\Controllers\PrimerControlador::class);
-//Route::resource('category',\App\Http\Controllers\PrimerControlador::class);
