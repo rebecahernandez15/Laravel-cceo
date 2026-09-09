5c760ef8941bf8d90cc31ff52367cd19375e820b
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-09 11:46:11 -0600

    feat(Registro): ✨We created a post to set up a registry.

--- database/migrations/2026_09_09_163222_create_post_table.php
+++ database/migrations/2026_09_09_163222_create_post_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('post', function (Blueprint $table) {
+        Schema::create('posts', function (Blueprint $table) {
             $table->id();
             $table->string('title', 500);
             $table->string('slug', 500);
@@ -31,6 +31,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('post');
+        Schema::dropIfExists('posts');
     }
 };
