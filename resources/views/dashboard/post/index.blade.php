5186b933f9cf16d848780ef27c735cab7a23c713
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-10 12:31:21 -0600

    feat (Listado): ✨ Once the form is filled out, it displays the provided information.

---
+++ resources/views/dashboard/post/index.blade.php
@@ -0,0 +1,32 @@
+@extends('dashboard.post.master')
+
+@section('content')
+    <table>
+        <thead>
+            <tr>
+                Title
+            </tr>
+            <tr>
+                Posted
+            </tr>
+            <tr>
+                Category
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $p)
+                <tr>
+                    <td>
+                        {{$p->title}}
+                    </td>
+                    <td>
+                        {{$p->posted}}
+                    </td>
+                    <td>
+                        {{$p->category->title}}
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+@endsection
