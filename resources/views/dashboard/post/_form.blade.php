f450c58d1b957985f6ff06b3b324648a369e83e1
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-10 16:36:53 -0600

    feat (Valores Anteriores): ✨ If a field fails one of the validations, the entered information will not be deleted.

--- resources/views/dashboard/post/_form.blade.php
+++ resources/views/dashboard/post/_form.blade.php
@@ -1,27 +1,27 @@
 @csrf
 <label for="title">Title</label>
-<input type="text" name="title" value="{{$post->title}}">
+<input type="text" name="title" value="{{ old('title', $post->title) }}">
 
 <label for="slug">Slug</label>
-<input type="text" name="slug" value="{{$post->slug}}">
+<input type="text" name="slug" value="{{ old('slug', $post->slug) }}">
 
 <label for="content">Content</label>
-<textarea name="content" >{{$post->content}}"</textarea>
+<textarea name="content">{{ old('content', $post->content) }}</textarea>
 
 <label for="">Category</label>
 <select name="category_id" id="category_id">
     @foreach($categories as $title => $id)
-        <option value="{{$id}}" {{$post->category_id == $id ? 'selected' : ''}} value="{{$id}}">{{$title}}</option>
+        <option value="{{$id}}" {{ old('category_id', $post->category_id) == $id ? 'selected' : '' }}>{{$title}}</option>
     @endforeach
 </select>
 
 <label for="description">Description</label>
-<textarea name="description" >{{$post->description}}</textarea>
+<textarea name="description">{{ old('description', $post->description) }}</textarea>
 
 <label for="posted">Posted</label>
 <select name="posted" id="posted">
-    <option {{$post->posted == 'yes' ? 'selected' : ''}} value="yes">Yes</option>
-    <option {{$post->posted == 'not' ? 'selected' : ''}} value="not">Not</option>
+    <option {{ old('posted', $post->posted) == 'yes' ? 'selected' : '' }} value="yes">Yes</option>
+    <option {{ old('posted', $post->posted) == 'not' ? 'selected' : '' }} value="not">Not</option>
 </select>
 
 <button type="submit">Send</button>
