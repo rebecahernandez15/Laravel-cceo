c78b4d87963648f8f82d34ec43b59d7eb4ece43c
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-10 09:32:49 -0600

    feat (CSRF): ✨ We learned about CSRF: it is a security vulnerability that tricks an authenticated user's browser into executing unwanted actions on a trusted web application.

--- app/Http/Controllers/Dashboard/PostController.php
+++ app/Http/Controllers/Dashboard/PostController.php
@@ -63,7 +63,7 @@ class PostController extends Controller
      */
     public function store(Request $request)
     {
-        return 'Store recibido';
+        dd(request()->get('title'));
     }
 
     /**
