53bf2e165320d0dfdd00da899e4ead020130c087
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-11 11:00:08 -0600

    feat (CRUD CATEGORY): ✨ A new category section with a new route was added.

---
+++ resources/views/dashboard/category/create.blade.php
@@ -0,0 +1,12 @@
+@extends('dashboard.post.master')
+
+@section('content')
+
+    @include('dashboard.fragment._errors-form')
+
+    <form action="{{ route('category.store') }}" method="POST">
+
+        @include('dashboard.category._form')
+
+    </form>
+@endsection
---
+++ resources/views/dashboard/category/show.blade.php
@@ -0,0 +1,8 @@
+@extends('dashboard.post.master')
+
+@section('content')
+
+    <h1>{{$category->title}}</h1>
+
+
+@endsection
