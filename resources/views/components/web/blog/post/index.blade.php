70aff5c701be1ea749907be1d3a251ef33f627aa
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-14 16:18:24 -0600

    feat(Parametros ): ✨Parameters changes

--- resources/views/components/web/blog/post/index.blade.php
+++ resources/views/components/web/blog/post/index.blade.php
@@ -1,3 +1,16 @@
 <div>
-    <h1> Hola mundo </h1>
+    <h1> Listado de Posts</h1>
+
+    @foreach ($posts as $p)
+
+        <div class="card card-white mb-2">
+            <h3>{{ $p->title }}</h3>
+            <a href="{{ route("web.blog.show",$p) }}">Ir</a>
+            <p>{{ $p->description }}</p>
+        </div>
+
+        {{ $posts->links() }}
+
+    @endforeach
+
 </div>
