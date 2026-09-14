dcb36b00e01d982863b87fd42e0ef17f1cedd242
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-14 11:08:04 -0600

    feat(Layout): ✨layout changes

--- resources/views/dashboard/post/index.blade.php
+++ resources/views/dashboard/post/index.blade.php
@@ -1,4 +1,4 @@
-@extends('dashboard.post.master')
+@extends('dashboard.master')
 
 @section('content')
 
@@ -15,31 +15,31 @@
         </tr>
         </thead>
         <tbody>
-            @foreach($posts as $p)
-                <tr>
-                    <td>
-                        {{$p->id}}
-                    </td>
-                    <td>
-                        {{$p->title}}
-                    </td>
-                    <td>
-                        {{$p->posted}}
-                    </td>
-                    <td>
-                        {{$p->category->title}}
-                    </td>
-                    <td>
-                        <a href="{{route('posts.edit',$p)}}">Edit</a>
-                        <a href="{{route('posts.show',$p)}}">Show</a>
-                        <form action = "{{route ('posts.destroy', $p)}}" method="post">
-                            @method('DELETE')
-                            @csrf
-                            <button type="submit">Delete</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
+        @foreach($posts as $p)
+            <tr>
+                <td>
+                    {{$p->id}}
+                </td>
+                <td>
+                    {{$p->title}}
+                </td>
+                <td>
+                    {{$p->posted}}
+                </td>
+                <td>
+                    {{$p->category->title}}
+                </td>
+                <td>
+                    <a href="{{route('posts.edit',$p)}}">Edit</a>
+                    <a href="{{route('posts.show',$p)}}">Show</a>
+                    <form action = "{{route ('posts.destroy', $p)}}" method="post">
+                        @method('DELETE')
+                        @csrf
+                        <button type="submit">Delete</button>
+                    </form>
+                </td>
+            </tr>
+        @endforeach
         </tbody>
     </table>
     {{$posts->links()}}
