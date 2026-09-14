ab6d02e8b28e5cfca329b8dca730262310753453
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-14 11:59:22 -0600

    feat(Contendor): ✨Cantainer changes

--- resources/views/dashboard/master.blade.php
+++ resources/views/dashboard/master.blade.php
@@ -7,13 +7,17 @@
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
-<body class="font-sans antialiased bg-gray-100">
-<div class="min-h-screen">
+<body class="font-sans antialiased">
+<div class="min-h-screen bg-gray-100">
     @include('layouts.navigation')
 
-    <main class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+    @if (session('status'))
+        {{ session('status') }}
+    @endif
+
+    <div class="container ">
         @yield('content')
-    </main>
+    </div>
 </div>
 </body>
 </html>
