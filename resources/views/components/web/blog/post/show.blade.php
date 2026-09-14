b4872d2d12f3eb0968414db036ce21f0de2e6169
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-14 17:11:06 -0600

    feat(Atributos): ✨Passed HTML attributes, class merging, and component method execution inside the Blade view.

--- resources/views/components/web/blog/post/show.blade.php
+++ resources/views/components/web/blog/post/show.blade.php
@@ -1,5 +1,7 @@
-{{$changeTittle()}}
-<h1>{{ $post->title }}</h1>
-<p>{{ $post->created_at }}</p>
-<p>{{ $post->content }}</p>
-
+<div {{ $attributes->class(['my-5', 'bg-blue-100' => false])->merge(['other-attr' => 'data1']) }}>
+    {{-- <div {{ $attributes->merge([ 'class' => 'my-5', 'other-attr' => 'data1' ]) }}> --}}
+    {{ $changeTitle() }}
+    <h1>{{ $post->title }}</h1>
+    <p>{{ $post->created_at }}</p>
+    <p>{{ $post->content }}</p>
+</div>
