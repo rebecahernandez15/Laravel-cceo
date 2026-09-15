ff98956095f1228b716e22568e78559a6e13f594
Rebeca De La Cruz <rebeca.delacruz@cceo.com.mx>
2026-09-14 18:09:00 -0600

    feat(seeders):✨ seeder para posts

--- database/seeders/CategorySeeder.php
+++ database/seeders/CategorySeeder.php
@@ -12,9 +12,7 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
-        Category::truncate();
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
+
 
         for ($i = 0; $i < 20; $i++) {
             Category::create(
--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,10 +18,6 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         $this->call(CategorySeeder::class);
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        $this->call(PostSeeder::class);
     }
 }
